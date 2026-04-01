f94700923259042ac02d170bc53d34face9f5884
filik19 <kozicka159@gmail.com>
2026-04-01 18:11:08 +0200

    Update redir.js

--- assets/scripts/redir.js
+++ assets/scripts/redir.js
@@ -1,1 +1,30 @@
+const params = new URLSearchParams(window.location.search);
+let site = params.get("site");
 
+const trusted = [
+    "youtube.com",
+    "youtu.be",
+    "discord.com",
+    "discord.gg",
+    "steamcommunity.com",
+    "store.steampowered.com"
+    "github.com"
+    "github.io"
+    "filik19.github.io"
+];
+
+try {
+    const url = new URL(site);
+    const host = url.hostname;
+
+    if (trusted.some(d => host.endsWith(d))) {
+        window.location.href = site;
+    } else {
+        document.getElementById("warning").style.display = "block";
+        document.getElementById("continue").onclick = () => {
+            window.location.href = site;
+        };
+    }
+} catch {
+    document.body.innerHTML = "url=invalid";
+}
